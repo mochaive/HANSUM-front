08f49942b4e09f4656ecbaea672fa42dd6f38566
whoisStarBox <y2142144@gmail.com>
2020-08-02 15:30:14 +0900

    기능: 라우팅 변경
    로그인 안 했을 시 무조건 "/auth"로 가도록 함
    회원가입 안 했다면 무조건 로그인 후 "/auth/register"로 가도록 함

---
+++ src/Main.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react"
+import { Route, Switch, Redirect } from "react-router-dom"
+import axios from "axios"
+
+import App from "./App"
+import Home from "./containers/Home"
+import Rank from "./containers/Rank"
+import User from "./containers/User"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
+const checkUser = async () => {
+    const response = await axios.get(`http://localhost:3001/routes/auth/check`)
+    return response.data
+}
+
+const Main = ({ match }) => {
+    useEffect(() => {
+        checkUser().then((data) => {
+            console.log("uid: " + data.uid)
+            localStorage.setItem("uid", data.uid)
+        })
+    }, [])
+
+    if (localStorage.getItem("uid") == "null") {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path="/auth" component={Login} />
+                    <Route path="/auth/register" component={Register} />
+                    <Route
+                        exact
+                        path="/"
+                        render={() => <Redirect to="/auth" />}
+                    />
+                </Switch>
+            </div>
+        )
+    } else if (match.url === "/") {
+        return (
+            <div>
+                <App />
+                <Route exact path="/" component={Home} />
+                <Route path="/rank" component={Rank} />
+                <Switch>
+                    <Route path="/user/:uid" component={User} />
+                    <Route path="/user" component={User} />
+                </Switch>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <Route exact path="/" component={Home} />
+                <Route path="/rank" component={Rank} />
+                <Switch>
+                    <Route path="/user/:uid" component={User} />
+                    <Route path="/user" component={User} />
+                </Switch>
+            </div>
+        )
+    }
+}
+
+export default Main
