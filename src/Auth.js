f1c612010e2277215b3d1c87f3a841e71bab0080
whoisStarBox <y2142144@gmail.com>
2020-08-03 21:30:45 +0900

    기능: Routing 수정

--- src/Auth.js
+++ src/Auth.js
@@ -1,45 +1,16 @@
-import React, { useEffect } from "react"
-import { Route, Switch, Redirect } from "react-router-dom"
-import axios from "axios"
+import React from "react"
+import { Route } from "react-router-dom"
 
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 
-const checkUser = async () => {
-    const response = await axios.get(`http://localhost:3001/routes/auth/check`)
-    return response.data
-}
-
 const Auth = ({ match }) => {
-    useEffect(() => {
-        checkUser().then((data) => {
-            console.log("uid: " + data.uid)
-            localStorage.setItem("uid", data.uid)
-        })
-    }, [])
-
-    if (localStorage.getItem("uid") === "null") {
-        return (
-            <div>
-                <Switch>
-                    <Route exact path="/auth" component={Login} />
-                    <Route path="/auth/register" component={Register} />
-                    <Route
-                        exact
-                        path="/"
-                        render={() => <Redirect to="/auth" />}
-                    />
-                </Switch>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <Route exact path="/auth" component={Login} />
-                <Route path="/auth/register" component={Register} />
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Route exact path="/auth" component={Login} />
+            <Route path="/auth/register" component={Register} />
+        </div>
+    )
 }
 
 export default Auth
