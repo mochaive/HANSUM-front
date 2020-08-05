9d35fb0858d258070f3890c627b20470b0ec9531
whoisStarBox <y2142144@gmail.com>
2020-08-05 15:00:27 +0900

    기능: WritePostPage 구현

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,9 +4,12 @@ import Header from "../../components/Header"
 import PostList from "../../components/PostList"
 
 const Home = ({ history }) => {
+    const onClick = () => {
+        history.push("/others/write")
+    }
     return (
         <div>
-            <Header type="Home" />
+            <Header type="Home" onClick={onClick} />
             <PostList type="Home" history={history} />
         </div>
     )
